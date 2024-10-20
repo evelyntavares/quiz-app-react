cb3acb240f13ff5c94d7213b3c1d1f7130f3a0b6
Evelyn Tavares <evelyn.tavares7@hotmail.com>
2024-10-20 15:59:15 -0600

    Adding color to the answer chosen, showing if the answer was correct or not, and reseting the timer

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -1,16 +1,46 @@
-export default function Answers({ answers, onSelectAnswer }) {
+export default function Answers({
+  answers,
+  answerStatus,
+  selectedAnswer,
+  onSelectAnswer,
+}) {
   return (
     <ul className="flex flex-col items-stretch gap-3 ">
-      {answers.map((answer) => (
-        <li>
-          <button
-            className="font-roboto bg-[#6cb7f5] hover:bg-[#9d5af5] hover:text-white rounded-3xl py-4 px-8 size-full"
-            onClick={() => onSelectAnswer(answer)}
-          >
-            {answer}
-          </button>
-        </li>
-      ))}
+      {answers.map((answer) => {
+        let cssClass =
+          'font-roboto hover:bg-[#9d5af5] hover:text-white rounded-3xl py-4 px-8 size-full';
+
+        const isSelected = selectedAnswer === answer;
+
+        if (isSelected) {
+          switch (answerStatus) {
+            case 'correct':
+              cssClass += ' bg-[#5af59d] text-[#2c203d]';
+              break;
+            case 'wrong':
+              cssClass += ' bg-[#f55a98] text-[#2c203d]';
+              break;
+            case 'answered':
+              cssClass += ' bg-[#f5a76c] text-[#2c203d]';
+            default:
+              break;
+          }
+        } else {
+          cssClass += ' bg-[#6cb7f5]';
+        }
+
+        return (
+          <li>
+            <button
+              className={cssClass}
+              onClick={() => onSelectAnswer(answer)}
+              disabled={answerStatus !== ''}
+            >
+              {answer}
+            </button>
+          </li>
+        );
+      })}
     </ul>
   );
 }
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,23 +1,70 @@
 import Answers from './Answers';
 import QUESTION from '../questions.js';
 import Timer from './Timer.jsx';
-
-const TIMEOUT = 10000;
+import { useState } from 'react';
 
 export default function Question({ questionIndex, onSelectAnswer }) {
   const question = QUESTION[questionIndex];
+
+  const [answerState, setAnswerState] = useState({
+    selectedAnswer: '',
+    isCorrect: null,
+  });
+
+  let timer = 10000;
+
+  if (answerState.selectedAnswer) {
+    timer = 1000;
+  }
+
+  if (answerState.isCorrect !== null) {
+    timer = 2000;
+  }
+
+  function handleSelectAnswer(answer) {
+    setAnswerState({
+      selectedAnswer: answer,
+      isCorrect: null,
+    });
+
+    setTimeout(() => {
+      setAnswerState({
+        selectedAnswer: answer,
+        isCorrect: answer === QUESTION[questionIndex].answers[0] ? true : false,
+      });
+
+      setTimeout(() => {
+        onSelectAnswer(answer);
+      }, 2000);
+    }, 1000);
+  }
+
+  let answerStatus = '';
+
+  if (answerState.selectedAnswer) {
+    answerStatus = 'answered';
+
+    if (answerState.isCorrect !== null) {
+      answerStatus = answerState.isCorrect ? 'correct' : 'wrong';
+    }
+  }
 
   return (
     <div>
       <Timer
         key={questionIndex}
-        timeout={TIMEOUT}
+        timeout={timer}
         onTimeout={() => onSelectAnswer(null)}
       />
       <h2 className="font-roboto text-2xl text-[#c1b2dd] font-bold mb-4 gap-4">
         {question.text}
       </h2>
-      <Answers answers={question.answers} onSelectAnswer={onSelectAnswer} />
+      <Answers
+        answers={question.answers}
+        answerStatus={answerStatus}
+        selectedAnswer={answerState.selectedAnswer}
+        onSelectAnswer={handleSelectAnswer}
+      />
     </div>
   );
 }
